14cafcb2af0c6ec31ffb2173f7482fe7f53d00cb
EsperanzaE <esperanza.berrocal@iesnervion.es>
2022-04-07 20:35:10 +0200

    nuevos ejemplos de Collections

--- src/Colecciones_Map/Main.java
+++ src/Colecciones_Map/Main.java
@@ -1,26 +1,36 @@
 package Colecciones_Map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 //declaramos un map con clave un entero y con valor un String
-static HashMap< Integer, String> miMapa= new HashMap<>();
+static Map< Integer, String> miLinkedHashMap= new LinkedHashMap<>();
+static Map< Integer, String> miHashMap= new HashMap<>();
+static Map< Integer, String> miTreeMap= new TreeMap<>();
 
     public static void main(String[] args) {
         //vamos a añadirle datos al mapa. Método put
-        miMapa.put( 1,"Juan");
-        miMapa.put(150,"Rafael");
-        miMapa.put(14,"Maria");
-        miMapa.put(5,"Pedro");
-        miMapa.put(9,"Ana");
+        miLinkedHashMap.put( 1,"Juan");
+        miLinkedHashMap.put(150,"Rafael");
+        miLinkedHashMap.put(14,"Maria");
+        miLinkedHashMap.put(5,"Pedro");
+        miLinkedHashMap.put(9,"Ana");
+        miHashMap.put( 1,"Juan");
+        miHashMap.put(150,"Rafael");
+        miHashMap.put(14,"Maria");
+        miHashMap.put(5,"Pedro");
+        miHashMap.put(9,"Ana");
+        miTreeMap.put( 1,"Juan");
+        miTreeMap.put(150,"Rafael");
+        miTreeMap.put(14,"Maria");
+        miTreeMap.put(5,"Pedro");
+        miTreeMap.put(9,"Ana");
 
         //Vamos a obtner el valor de la clave 14. Método get
-        System.out.println("el valor de la clave 14 es "+ miMapa.get(14));
+        System.out.println("el valor de la clave 14 es "+ miHashMap.get(14));
 
         //vamos a obtener una coleccion Set con todas las claves del Map
-        Set<Integer> clavesDeMiMapa= miMapa.keySet();
+        Set<Integer> clavesDeMiMapa= miHashMap.keySet();
         //lo recorro con foreach
 
         System.out.println("recorro las claves del mapa con un foreach");
@@ -29,10 +39,19 @@ static HashMap< Integer, String> miMapa= new HashMap<>();
             System.out.println("la clave "+ i + " es " +elemento);
             i++;
         }
-        System.out.println("vemos que están ordenadas aunque no la hemos insertado de forma ordenada ");
-        System.out.println("un vistazo al mapa tal y como está. Método toString ");
-        System.out.println(miMapa);
+        System.out.println("muestro las tres implementaciones de Map para ver cuál esta ordenada");
+        System.out.println();
 
+        System.out.println("HASHMAP");
+        System.out.println(miHashMap);
+        System.out.println();
+        System.out.println("LINKEDHASHMAP");
+        System.out.println(miLinkedHashMap);
+        System.out.println();
+        System.out.println("TREEMAP");
+        System.out.println(miTreeMap);
+        System.out.println();
+//como vemos solo ordena automaticamente TreeMap
     }
 }
 
